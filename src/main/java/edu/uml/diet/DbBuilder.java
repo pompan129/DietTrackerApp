6203c8f4535334cadaea242a601e93cfc0803308
rgoolishian <raymondgoolishian@yahoo.com>
2015-02-24 16:17:24 -0500

    Corrected exception handling issues in DbParser and DbParserTest

--- src/main/java/edu/uml/diet/DbBuilder.java
+++ src/main/java/edu/uml/diet/DbBuilder.java
@@ -133,7 +133,9 @@ public class DbBuilder {
             String sql = "CREATE TABLE USERS (" +
                     "id INTEGER not NULL, " +
                     "first_name VARCHAR(255)," +
-                    "last_name VARCHAR(255))";
+                    "last_name VARCHAR(255))" +
+                    "username VARCHAR(255))" +
+                    "password VARCHAR(255))";
             stmt.executeUpdate(sql);
         }
         finally{
