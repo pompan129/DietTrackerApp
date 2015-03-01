59659888a44a4d77df4be355a7f75128129517b1
Adil Sadik <sparky.005@gmail.com>
2015-03-01 15:51:40 -0500

    edited web.xml, servlet, login.html

--- src/main/java/edu/uml/diet/LoginServlet.java
+++ src/main/java/edu/uml/diet/LoginServlet.java
@@ -3,21 +3,32 @@ package edu.uml.diet;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import edu.uml.diet.*;
+
 
 /**
  * Created by adil on 3/1/15.
  */
-public class loginServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 
     private String email = "";
     private String password = "";
+    private PersistanceUserServices persistanceUserService;
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         email = request.getParameter("email");
         password = request.getParameter("password");
-        out.println(email + '\n' + password);
+
+        BasicUserService basicUserService = new BasicUserService(persistanceUserService);
+        boolean authentication = basicUserService.verifyUser(email, password);
+        if(authentication){
+            out.println("HI");
+        }
+        else {
+            out.println(email + '\n' + password);
+        }
 
 
 
