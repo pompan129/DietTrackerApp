eb78cb2080b794e95253a64bd70b0706fa9deb42
Adil Sadik <sparky.005@gmail.com>
2015-03-01 19:52:01 -0500

    working login and registration servlets

--- src/main/java/edu/uml/diet/LoginServlet.java
+++ src/main/java/edu/uml/diet/LoginServlet.java
@@ -3,8 +3,6 @@ package edu.uml.diet;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
-import edu.uml.diet.*;
-
 
 /**
  * Created by adil on 3/1/15.
@@ -13,24 +11,29 @@ public class LoginServlet extends HttpServlet {
 
     private String email = "";
     private String password = "";
-    private PersistanceUserServices persistanceUserService;
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n" +
+                "        \"http://www.w3.org/TR/html4/loose.dtd\">");
         email = request.getParameter("email");
         password = request.getParameter("password");
 
-        BasicUserService basicUserService = new BasicUserService(persistanceUserService);
-        boolean authentication = basicUserService.verifyUser(email, password);
-        if(authentication){
-            out.println("HI");
+        BasicUserService basicUserService = new BasicUserService(new DbUserServices());
+        boolean authenticated = false;
+        try {
+            authenticated = basicUserService.verifyUser(email, password);
+        } catch (PersistanceUserServicesException e) {
+            e.printStackTrace();
         }
-        else {
-            out.println(email + '\n' + password);
+        //TODO: Proper formatting/HTML output
+        if (authenticated) {
+            out.println("<p>HI</p>");
+        } else {
+            out.println("<p>Username not found. Do you want to <a href = \"register.html\"> register</a>?</p>");
         }
-
-
-
     }
 }
---
+++ src/main/java/edu/uml/diet/RegistrationServlet.java
@@ -0,0 +1,57 @@
+package edu.uml.diet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+/**
+ * Created by adil on 3/1/15.
+ */
+public class RegistrationServlet extends HttpServlet {
+
+    private String email = "";
+    private String password = "";
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n" +
+                "        \"http://www.w3.org/TR/html4/loose.dtd\">");
+        email = request.getParameter("email");
+        password = request.getParameter("password");
+
+        BasicUserService basicUserService = new BasicUserService(new DbUserServices());
+        //TODO: remove
+        DatabaseBuilder builder = new DatabaseBuilder(new DatabaseConnector(), "DietTracker");
+        try {
+            if(!builder.CheckIfDbExists()) {
+                builder.CreateDatabase();
+            }
+        } catch (DatabaseConnectorException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(!builder.CheckIfTableExists("USERS")) {
+                builder.CreateUserTable();
+            }
+        } catch (DatabaseConnectorException e) {
+            e.printStackTrace();
+        }
+
+        //TODO: End remove
+
+        try {
+            basicUserService.createUser(email, password);
+            out.println("successfully created. You will now be directed back to login");
+        } catch (PersistanceUserServicesException e) {
+            e.printStackTrace(out);
+        } catch (DuplicateUserException e) {
+            e.printStackTrace(out);
+        }
+
+
+    }
+
+
+}
