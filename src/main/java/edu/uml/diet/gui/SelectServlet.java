686c109fb36a0a8f64273cbe044af436279b2037
Adil Sadik <sparky.005@gmail.com>
2015-03-15 18:38:52 -0400

    can now select foods from search page

--- src/main/java/edu/uml/diet/gui/SelectServlet.java
+++ src/main/java/edu/uml/diet/gui/SelectServlet.java
@@ -24,13 +24,48 @@ public class SelectServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        double portionSize = 0;
-        String portionID = "";
         PrintWriter out = response.getWriter();
-        Enumeration<String> parameterNames = request.getParameterNames();
-        String parameterName = parameterNames.nextElement();
-        out.println(request.getParameter("selected"));
+        HttpSession session = request.getSession(false);
+        String[] portionIDs = request.getParameterValues("portionID");
+
+        double portionSize = 1.0;
+        String portionID = "testFood";
+        FoodService foodService;
+        Portion portion;
+        ArrayList<Portion> userPortionList = (ArrayList<Portion>) session.getAttribute("userPortionList");
+
+        try {
+            foodService = ServiceFactory.getFoodServiceInstance();
+        } catch (FoodServiceException e) {
+            throw new ServletException("SearchServlet Error when creating foodService: ", e);
+        }
+
+        if(portionIDs != null) {
+            for (String selected : portionIDs) {
+                portionSize = Integer.parseInt(request.getParameter("portionSize"));
+                portionID = selected;
+                try {
+                    portion = foodService.foodSearch(portionID);
+                } catch (FoodServiceException e) {
+                    throw new ServletException("Error searching for single food: ", e);
+                }
+                portion.setPortionSize(portionSize);
+                userPortionList.add(portion);
+            }
+            session.setAttribute("userPortionList", userPortionList);
+        }
+        else {
+            out.println("error: nothing selected");
+        }
+
+        for(int i = 0; i<userPortionList.size(); i++) {
+            out.println(userPortionList.get(i).getFood().getName());
+            out.println(userPortionList.get(i).getCalories());
+        }
+
+        //Enumeration<String> parameterNames = request.getParameterNames();
+        //String parameterName = parameterNames.nextElement();
+        //out.println(request.getParameter("selected"));
         /*
         while(parameterNames.hasMoreElements()) {
             String parameterName = parameterNames.nextElement();
@@ -42,7 +77,7 @@ public class SelectServlet extends HttpServlet {
             }
         }
         */
-
+/*
         FoodService foodService;
         try {
             foodService = ServiceFactory.getFoodServiceInstance();
@@ -65,9 +100,7 @@ public class SelectServlet extends HttpServlet {
             out.println(userPortionList.get(i).getCalories());
         }
         session.setAttribute("userPortionList", userPortionList);
-        ArrayList<String> test = new ArrayList<>();
-        test.add("HI");
 
-        out.println(test.get(0));
+        out.println(test.get(0)); */
     }
 }
--- src/main/webapp/WEB-INF/search.jsp
+++ src/main/webapp/WEB-INF/search.jsp
@@ -29,11 +29,10 @@ Food: <input type="text" name="query" />
     <form name="foodSelect" action="select" method="POST">
     <c:forEach items="${portionList}" var="portion">
         <tr>
-            <td><input type = "checkbox" name="selected" /> </td>
+            <td><input type = "checkbox" name="portionID" value=<c:out value="${portion.getFood().getName()}" /> </td>
             <td><c:out value="${portion.getFood().getName()}" /></td>
             <td><c:out value="${portion.getFood().getCalories()}" /></td>
             <td><input type = "text" name="portionSize" size="2"/></td>
-            <td><input type = "hidden" name="portionID" value=<c:out value="${portion.getFood().getName()}" /> /> </td>
         </tr>
     </c:forEach>
     <input type = "submit" />
