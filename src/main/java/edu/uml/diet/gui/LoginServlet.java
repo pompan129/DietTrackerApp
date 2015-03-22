d5511ac743b630a7b7bd31b85e03a2f324a5bb5d
Adil Sadik <sparky.005@gmail.com>
2015-03-22 15:48:26 -0400

    fixed import statements

--- src/main/java/edu/uml/diet/gui/LoginServlet.java
+++ src/main/java/edu/uml/diet/gui/LoginServlet.java
@@ -6,14 +6,13 @@ import edu.uml.diet.logic.UserServiceException;
 import edu.uml.diet.model.Day;
 import edu.uml.diet.model.Meal;
 import edu.uml.diet.model.Portion;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
-import java.util.ArrayDeque;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 /**
  * Servlet for handling user login attempts
