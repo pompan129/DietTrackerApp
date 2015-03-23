379e2c7d262ab347659a91c729ef37b000410b15
fazbat <fazbat@yahoo.com>
2015-03-22 22:30:17 -0700

    Daytest stuff

--- src/test/java/edu/uml/diet/model/DayTest.java
+++ src/test/java/edu/uml/diet/model/DayTest.java
@@ -8,7 +8,6 @@ import java.sql.Date;
 import java.util.ArrayList;
 
 public class DayTest extends TestCase {
-
     Meal meal_1;
     Meal meal_2;
     int id;
@@ -23,6 +22,8 @@ public class DayTest extends TestCase {
     double householdWeight;
     String householdWeightDescription;
     Day day;
+    Date date;
+    User user;
 
     @Before
     public void setUp() {
@@ -43,47 +44,77 @@ public class DayTest extends TestCase {
         meal_1.setPortion(portion_1);
         meal_2 = new Meal();
         meal_2.setPortion(portion_2);
-
-
+        date = Date.valueOf("2015-3-21");
+        user = new User();
+        user.setId(7);
     }
 
     @Test
     public void testGetId() throws Exception {
+        day = new Day();
+        day.setId(id);
+        assertTrue("ids are equal", id == day.getId());
 
     }
 
     @Test
-
     public void testSetId() throws Exception {
-
+        day = new Day();
+        day.setId(id);
+        assertTrue("ids are equal", id == day.getId());
     }
 
     @Test
     public void testGetDate() throws Exception {
-
+        day = new Day();
+        day.setDate(date);
+        assertEquals("dates are equal", date, day.getDate());
     }
 
     @Test
     public void testSetDate() throws Exception {
+        day = new Day();
+        day.setDate(date);
+        assertEquals("dates are equal", date, day.getDate());
 
     }
 
+    @Test
     public void testGetMeals() throws Exception {
+        ArrayList<Meal> mealArrayList = new ArrayList<>();
+        mealArrayList.add(meal_1);
+        mealArrayList.add(meal_2);
+        day = new Day();
+        day.setMeals(mealArrayList);
+        assertTrue("contains meal 1",mealArrayList.contains(meal_1));
+        assertTrue("contains meal 2",mealArrayList.contains(meal_2));
 
     }
 
+    @Test
     public void testSetMeals() throws Exception {
+        ArrayList<Meal> mealArrayList = new ArrayList<>();
+        mealArrayList.add(meal_1);
+        mealArrayList.add(meal_2);
+        day = new Day();
+        day.setMeals(mealArrayList);
+        assertTrue("contains meal 1",mealArrayList.contains(meal_1));
+        assertTrue("contains meal 2",mealArrayList.contains(meal_2));
 
     }
 
     @Test
     public void testGetUser() throws Exception {
-
+        day = new Day();
+        day.setUser(user);
+        assertEquals("user are equal", user, day.getUser());
     }
 
     @Test
     public void testSetUser() throws Exception {
-
+        day = new Day();
+        day.setUser(user);
+        assertEquals("user are equal", user, day.getUser());
     }
 
     @Test
@@ -95,16 +126,43 @@ public class DayTest extends TestCase {
         day.setMeals(mealArrayList);
         int calories = meal_1.getCalories() + meal_2.getCalories();
         assertTrue("calories are equal", calories == day.getCalories());
-
     }
 
     @Test
     public void testEquals() throws Exception {
+        day = new Day();
+        Day dayCopy = new Day();
+        ArrayList<Meal> mealArrayList= new ArrayList<>();
+        mealArrayList.add(meal_1);
+        mealArrayList.add(meal_2);
+        day.setMeals(mealArrayList);
+        day.setDate(date);
+        day.setId(id);
+        day.setUser(user);
+        dayCopy.setMeals(mealArrayList);
+        dayCopy.setDate(date);
+        dayCopy.setId(id);
+        dayCopy.setUser(user);
+        assertTrue("days are equal", day.equals(dayCopy));
 
     }
 
     @Test
     public void testHashCode() throws Exception {
+        day = new Day();
+        Day dayCopy = new Day();
+        ArrayList<Meal> mealArrayList= new ArrayList<>();
+        mealArrayList.add(meal_1);
+        mealArrayList.add(meal_2);
+        day.setMeals(mealArrayList);
+        day.setDate(date);
+        day.setId(id);
+        day.setUser(user);
+        dayCopy.setMeals(mealArrayList);
+        dayCopy.setDate(date);
+        dayCopy.setId(id);
+        dayCopy.setUser(user);
+        assertTrue("hashcode is equal", day.hashCode() == dayCopy.hashCode());
 
     }
 }
