5b6593dea6564880a515c7b9fe7154ee58351023
Adil Sadik <sparky.005@gmail.com>
2015-03-31 21:44:20 -0400

    other select servlet tests

--- src/test/java/edu/uml/diet/gui/SelectServletTest.java
+++ src/test/java/edu/uml/diet/gui/SelectServletTest.java
@@ -2,8 +2,11 @@ package edu.uml.diet.gui;
 
 import edu.uml.diet.logic.FoodService;
 import edu.uml.diet.logic.ServiceFactory;
+import edu.uml.diet.model.Day;
+import edu.uml.diet.model.Meal;
 import edu.uml.diet.model.Portion;
 import junit.framework.TestCase;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -27,6 +30,9 @@ public class SelectServletTest extends TestCase {
     private MockHttpSession session;
     SelectServlet selectServlet;
     List<String> portionSizesParsed;
+    String email;
+    Day day;
+    String mealID;
 
     @Before
     public void setUp() throws Exception {
@@ -39,42 +45,54 @@ public class SelectServletTest extends TestCase {
         response = new MockHttpServletResponse();
         session = new MockHttpSession();
 
+        email = "123";
+
         selectServlet = new SelectServlet();
 
         foodService = ServiceFactory.getFoodServiceInstance();
+        day = foodService.getDay(email, DateTime.now());
         userPortionList =  new ArrayList<>();
         session.setAttribute("loggedIn", true);
         session.setAttribute("userPortionList", userPortionList);
         session.setAttribute("foodService", foodService);
+        session.setAttribute("day", day);
         request.setParameter("portionIDs", portionIDs);
         request.setParameter("portionSize", portionSizes);
+        request.setParameter("mealID", "1");
         request.setSession(session);
     }
 
+
     @Test
-    public void testDoGet() throws Exception {
-        selectServlet.doGet(request, response);
+    public void testDoPost() throws Exception {
+        selectServlet.doPost(request, response);
         assertEquals("/WEB-INF/select.jsp", response.getForwardedUrl());
     }
 
     @Test
-    public void testDoPost() throws Exception {
-
-    }
-
-    @Test
     public void testSetSelectedPortions() throws Exception {
-        selectServlet.setSelectedPortions(request, response, session, portionIDs, portionSizesParsed, foodService, userPortionList);\
-
+        selectServlet.setSelectedPortions(request, response, session, portionIDs, portionSizesParsed, foodService, userPortionList);
+        assertEquals("/WEB-INF/select.jsp", response.getForwardedUrl());
     }
 
     @Test
     public void testGetAndUpdateDay() throws Exception {
+        selectServlet.setSelectedPortions(request, response, session, portionIDs, portionSizesParsed, foodService, userPortionList);
+        Day newDay = selectServlet.getAndUpdateDay(request, session);
 
+        int mealID = 0;
+        ArrayList<Meal> meals = new ArrayList<>(day.getMeals());
+        Meal userMeal = meals.get(mealID);
+        userMeal.setPortions(userPortionList);
+
+        ArrayList<Meal> newMeals = new ArrayList<>(newDay.getMeals());
+        Meal newMeal = newMeals.get(mealID);
+
+        assertEquals(userMeal, newMeal);
     }
 
 
-    protected List<String> parsePortionList(String[] portionSizes) {
+    private List<String> parsePortionList(String[] portionSizes) {
         //parse portionSizes to remove empty entries
         List<String> portionSizesParsed = new ArrayList<>(Arrays.asList(portionSizes));
         portionSizesParsed.removeAll(Arrays.asList("", null));
